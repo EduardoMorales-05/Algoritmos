98b3285b7727181465b9d881406db86e9f311b40
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2026-04-26 21:56:03 -0500

    Create insertarAlFinal.java

---
+++ Segunda Fase/Ejercicios/Ejercicio 3/insertarAlFinal.java
@@ -0,0 +1,20 @@
+public static <T> Node<T> insertarAlFinal(Node<T> head, T valor)
+{
+    Node<T> nuevo = new Node<>(valor);
+
+    if(head == null)
+    {
+        return nuevo;
+    }
+
+    Node<T> actual = head;
+
+    while(actual.next != null)
+    {
+        actual = actual.next;
+    }
+
+    actual.next = nuevo;
+
+    return head;
+}
