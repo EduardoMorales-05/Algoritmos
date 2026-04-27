24fdb5b6b8d373442a7e2fde0e4bee6c40641c29
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2026-04-26 21:54:11 -0500

    Add files via upload

---
+++ Segunda Fase/Ejercicios/Ejercicio 1/buscarElemento.java
@@ -0,0 +1,16 @@
+public static <T> boolean buscarElemento(ListLinked<T> lista, T valor)
+{
+    Node<T> actual = lista.getFirst();
+
+    while(actual != null)
+    {
+        if(actual.value.equals(valor))
+        {
+            return true;
+        }
+
+        actual = actual.next;
+    }
+
+    return false;
+}
