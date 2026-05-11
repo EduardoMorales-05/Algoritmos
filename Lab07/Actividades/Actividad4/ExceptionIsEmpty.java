a01121ff79522741d6dfb715e1ce4cf3a30d2ff5
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2026-05-10 22:14:37 -0500

    Create ExceptionIsEmpty.java

---
+++ Lab07/Actividades/Actividad4/ExceptionIsEmpty.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+/**
+ * Valida que esté vacía la estructura de datos.
+ */
+public class ExceptionIsEmpty extends Exception {
+    public ExceptionIsEmpty(String msg) {
+        super(msg);
+    }
+    public ExceptionIsEmpty() {
+        super();
+    }
+}
