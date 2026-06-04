63e435f0ca3abed30f9ac62b7255802e23b2e3f5
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2026-06-04 07:48:37 -0500

    Create main.java

---
+++ lab09/Actividades/Actividad 3/main.java
@@ -0,0 +1,15 @@
+package btree;
+
+public class main {
+	public static void main(String[] args) {
+	    BTree<Integer> arbol = new BTree<>(4);
+	    int[] datos = {
+	        31,12,19,3,10,13,16,22,25,28,
+	        41,57,63,33,35,40,49,52,55,
+	        60,62,67,70,72
+	    };
+	    for (int x : datos)
+	        arbol.insert(x);
+	    System.out.println(arbol);
+	}
+}
