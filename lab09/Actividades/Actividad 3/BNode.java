abfd1b403aa0778074f5a95b58d8770ee3166285
EduardoMoralesUCSM <Eduardo.morales@estudiante.ucsm.edu.pe>
2026-06-04 07:48:00 -0500

    Update BNode.java

--- lab09/Actividades/Actividad 3/BNode.java
+++ lab09/Actividades/Actividad 3/BNode.java
@@ -7,8 +7,11 @@ public class BNode<E extends Comparable<E>> {
     protected ArrayList<E> keys;
     protected ArrayList<BNode<E>> childs;
     protected int count;
+
     private static int nextId = 1;
     protected int idNode;
+
+    protected BNode<E> parent;
 
     public BNode(int n) {
         this.keys = new ArrayList<E>(n);
@@ -16,44 +19,34 @@ public class BNode<E extends Comparable<E>> {
         this.count = 0;
 
         this.idNode = nextId++;
+        this.parent = null;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             keys.add(null);
-        }
 
-        for (int i = 0; i < n + 1; i++) {
+        for (int i = 0; i < n + 1; i++)
             childs.add(null);
-        }
     }
 
-
-    public boolean nodeFull() {
-        return count == keys.size();
+    public boolean nodeFull(int orden) {
+        return count == orden - 1;
     }
-
 
     public boolean nodeEmpty() {
         return count == 0;
     }
 
-
     public boolean searchNode(E key, int[] pos) {
 
         int i = 0;
 
-        while (i < count && key.compareTo(keys.get(i)) > 0) {
+        while (i < count && key.compareTo(keys.get(i)) > 0)
             i++;
-        }
 
         pos[0] = i;
 
-        if (i < count && key.compareTo(keys.get(i)) == 0) {
-            return true;
-        }
-
-        return false;
+        return i < count && key.compareTo(keys.get(i)) == 0;
     }
-
 
     @Override
     public String toString() {
@@ -65,9 +58,8 @@ public class BNode<E extends Comparable<E>> {
         for (int i = 0; i < count; i++) {
             sb.append(keys.get(i));
 
-            if (i < count - 1) {
+            if (i < count - 1)
                 sb.append(", ");
-            }
         }
 
         sb.append("]");
